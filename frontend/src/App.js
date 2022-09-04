f42fc6255bc4eba8284b0b062862550ceacf59ac
GaboGomezT <gomezgabriel09@gmail.com>
2022-09-04 15:35:22 -0500

    commented out unused code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
 import DrawingTool from './components/drawingtool';
-import PencilTool from './components/penciltool';
+// import PencilTool from './components/penciltool';
 
 function App() {
   return (
     <div className='App'>
-      <DrawingTool /> //Change this line to <PencilTool /> to render pencil
+      <DrawingTool />
     </div>
   );
 }
